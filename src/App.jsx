47b4f7082842060c3cfcd3f0560082a3fc3ba258
Khageswar-Nayak <khageswarnayak045@gmail.com>
2023-12-24 23:55:07 +0530

    heroBannerSection

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,24 @@ function App() {
   const url = useSelector((state) => state.home.url);
 
   useEffect(() => {
-    apiTest();
+    fetchApiConfig();
   }, []);
 
-  const apiTest = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
     });
   };
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
@@ -36,6 +42,7 @@ function App() {
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
